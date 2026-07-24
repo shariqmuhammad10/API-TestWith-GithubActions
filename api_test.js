b6a731cf7722791c2e28476c4909aa70cdd5a2de
Shariq Muhammad <20771456+shariqmuhammad10@users.noreply.github.com>
2026-07-24 16:17:13 +0500

    test update

--- api_test.js
+++ api_test.js
@@ -3,11 +3,11 @@ import { check, sleep } from 'k6';
 
 // 1. Define Ops-style thresholds (The test fails if these are missed)
 export const options = {
-  vus: 5,           // 5 virtual users simulated at once
-  duration: '10s',  // Run the test continuously for 10 seconds
+  vus: 3,           // 5 virtual users simulated at once
+  duration: '5s',  // Run the test continuously for 10 seconds
   thresholds: {
-    http_req_duration: ['p(95)<1000'], // 95% of requests must be faster than 500ms
-    http_req_failed: ['rate<0.01'],   // Error rate must be less than 1%
+    http_req_duration: ['p(95)<2000'], // 95% of requests must be faster than 500ms
+    http_req_failed: ['rate<0.05'],   // Error rate must be less than 1%
   },
 };
 
